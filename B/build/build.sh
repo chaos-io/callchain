4a6508a6788b03c082e05af6300b1d644fa855ec
Eric <>
2023-01-12 18:42:31 +0800

    add build script

---
+++ B/build/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -x
+pwd=$(cd $(dirname $0); pwd)
+basedir=$(dirname $pwd)
+tag=$(cat $(dirname $basedir)/version)
+
+remotePrefix=ericyami
+imageName=callchain-b
+
+docker build -t $remotePrefix/$imageName:$tag -f $pwd/Dockerfile $basedir
+docker tag $remotePrefix/$imageName:$tag $remotePrefix/$imageName:latest
+docker push $remotePrefix/$imageName:$tag
+docker push $remotePrefix/$imageName:latest
