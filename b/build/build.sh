4e7265adb7220c7ba6eadb54c9fc734fab002286
Eric Yue <yuekewin@gmail.com>
2024-05-30 17:35:50 +0800

    ci: update build.sh

--- b/build/build.sh
+++ b/build/build.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 set -x
-pwd=$(cd $(dirname $0); pwd)
-basedir=$(dirname $pwd)
-tag=$(cat $(dirname $basedir)/version)
+
+case "$1" in
+"amd64" | "arm64") ARCH="$1" ;;
+"") ARCH="amd64" ;;
+*) echo "Unknown 1st option, ARCH:$1" && exit 1 ;;
+esac
+
+pwd=$(cd "$(dirname "$0")" && pwd)
+basedir=$(dirname "$pwd")
+tag=$(cat "$(dirname "$basedir")/version")
 
 remotePrefix=ericyami
 imageName=callchain-b
 
-docker build -t $remotePrefix/$imageName:$tag -f $pwd/Dockerfile $basedir
-docker tag $remotePrefix/$imageName:$tag $remotePrefix/$imageName:latest
-docker push $remotePrefix/$imageName:$tag
+docker build --platform linux/"$ARCH" -f "$pwd"/Dockerfile -t $remotePrefix/$imageName:"$tag" "$basedir"
+docker tag $remotePrefix/$imageName:"$tag" $remotePrefix/$imageName:latest
+docker push $remotePrefix/$imageName:"$tag"
 docker push $remotePrefix/$imageName:latest
